c39519e23ba2ab49e0b07ced6d54a112821d56b5
pado <antoine.padovani@synchrotron-soleil.fr>
2022-04-02 01:13:13 +0200

    api curation

--- lib/tess/api/api_resource.rb
+++ lib/tess/api/api_resource.rb
@@ -48,6 +48,42 @@ module Tess
         !response['id'].nil?
       end
 
+      #we can refactor the unscraped and eventunscraped functions beneath in a single one like this
+      #this current refactor does not work, todo in the future if we want to clean up the code
+      #def unscraped?                
+      #  if self.class.data_type == :event
+      #      type = "eventunscraped"
+      #  elsif self.class.data_type== :material
+      #     type = "unscraped"
+      #  end  
+              
+      #  data = {type:{"url": self.url}}
+      #  response = Uploader.lookup_unscraped(data, "/"+type+"s"+"/check_exists.json", :post)
+      #  self.id = response['id']
+
+      #  !response['id'].nil?
+      #end
+
+
+      #these work fine, tested them
+      def unscraped?
+        #puts self.url
+        data = {"unscraped":{"url": self.url}}
+        response = Uploader.lookup_unscraped(data, "/unscrapeds/check_exists.json", :post)
+        self.id = response['id']
+              
+        !response['id'].nil?
+      end
+
+      def eventunscraped?
+        #puts self.url
+        data = {"eventunscraped":{"url": self.url}}
+        response = Uploader.lookup_unscraped(data, "/eventunscrapeds/check_exists.json", :post)
+        self.id = response['id']
+      
+        !response['id'].nil?
+      end
+
       private
 
       def error_message(exception)
--- lib/tess/api/uploader.rb
+++ lib/tess/api/uploader.rb
@@ -4,15 +4,27 @@ module Tess
 
       def self.do_upload(data, auth, data_type, action, method)
         payload = { data_type => data.dump }
-
+        
         if auth
           payload[:user_email] = Tess::API.config['user_email']
           payload[:user_token] = Tess::API.config['user_token']
         end
-
+        
         response = RestClient::Request.execute(method: method.to_sym,
                                                url: (Tess::API.base_url + action),
                                                payload: payload.to_json,
+                                               headers: { content_type: 'application/json'})
+        begin
+          JSON.parse(response.body)
+        rescue JSON::ParserError
+          {}
+        end
+      end
+ 
+      def self.lookup_unscraped(data, action, method)                 
+        response = RestClient::Request.execute(method: method.to_sym,
+                                               url: (Tess::API.base_url + action),
+                                               payload: data,
                                                headers: { content_type: 'application/json'})
         begin
           JSON.parse(response.body)
